2fbe8ee8d1ea3dc5098cf214b2cab17590965fa0
kris <692365092-png@users.noreply.github.com>
2026-09-02 21:10:07 +0800

    Tidy PetForm composition usings

--- desktop-pet/PetForm.cs
+++ desktop-pet/PetForm.cs
@@ -1,13 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Windows.Forms;
 
